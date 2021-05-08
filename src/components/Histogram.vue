7a24618ae62e74668c72100bbabcc3abd345bf2c
coderjones <coderjones@fastmail.com>
2021-05-08 14:31:48 -0500

    Update Historgram with some details

--- src/components/Histogram.vue
+++ src/components/Histogram.vue
@@ -1,5 +1,48 @@
 <template>
   <v-container>
+      <section>
+
+        <h4 class="v-heading text-h6 mb-2">Histogram </h4> <p>And example of using a histogram with random data. </p>
+      </section>
+    <v-row>
+        <v-col cols="12">
+            <h4>Markup:</h4>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col cols="12">
+            <code>
+                {{markup}}
+            </code>
+        </v-col>
+    </v-row> 
+    <v-row>
+        <v-col cols="12">
+            <h4>Code:</h4>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col cols="12">
+            <pre>
+            
+<code>         computed: {
+                myData: function() {
+                        var x = [];
+                        for (var i = 0; i &lt; 500; i ++) {
+                            x[i] = Math.random();
+                        }
+
+                        var trace = {
+                            x: x,
+                            type: 'histogram',
+                        };
+                        return [trace];
+                    }
+                }
+    }</code>
+            </pre>
+        </v-col>
+    </v-row>     
     <v-row class="text-center">
       <v-col cols="12">
         <Plotly :data="myData" :layout="layout"></Plotly>
@@ -10,6 +53,7 @@
         <div id='myDiv' style="width:600px;height:250px;"></div>
       </v-col>
     </v-row>
+   
   </v-container>
 </template>
 
@@ -40,7 +84,22 @@
     data:[],
     layout:{
       title: "Histogram"
-    }
+    },
+    markup: '<Plotly :data="myData" :layout="layout"></Plotly>',
+    exampleCode: '    computed: {<br>\
+        myData: function() {<br>\
+            var x = [];<br>\
+            for (var i = 0; i < 500; i ++) {<br>\
+                x[i] = Math.random();<br>\
+            }<br>\
+<br>\
+            var trace = {<br>\
+                x: x,<br>\
+                type: \'histogram\',<br>\
+            };<br>\
+            return [trace];<br>\
+        }<br>\
+    }'
     }),
   }
 </script>
