456c3dbdd61ca53372de73c4d4f355919070f28e
coderjones <coderjones@fastmail.com>
2021-05-08 14:40:05 -0500

    Add link to vue-plotly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,11 @@
         <h3>Pick a chart example from the left navigation</h3>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-label><a href="https://github.com/statnett/vue-plotly" target="_blank">View the vue-plotly component here.</a></v-label>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
