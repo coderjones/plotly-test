fc53284a654ad09752427bf7f2ef2722294e8c08
coderjones <coderjones@fastmail.com>
2021-06-05 13:03:53 -0500

    Update app.js

--- webapi/app.js
+++ webapi/app.js
@@ -1,6 +1,39 @@
 const express = require('express');
+let bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/products", (req,res) => {
+    const products = [
+    {
+      id: 1,
+      name: "hammer",
+    },
+    {
+      id: 2,
+      name: "screwdriver",
+    },
+    ,
+    {
+      id: 3,
+      name: "wrench",
+    },
+   ];
+  
+   res.json(products);
+  });
+
+  app.get('/products/:id', (req, res) => {
+    // handle this request `req.params.id`
+  })
+
+  app.post('/<path>', (req, res) => {
+    console.log('req.body', req.body) // contains incoming data
+  })
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
